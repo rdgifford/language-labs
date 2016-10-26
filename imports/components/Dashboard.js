21cc15847947b500cbed29ee0b7a72de6abc6eaf
Hack Reactor Students <accounts@hackreactor.com>
2016-10-25 18:18:32 -0700

    Add button to dashboard to conditionally render call logic or review logic

--- imports/components/Dashboard.js
+++ imports/components/Dashboard.js
@@ -10,6 +10,10 @@ import TopicSuggestion   from './TopicSuggestion';
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      inCall: true
+    };
   }
 
   startChat(user, peer) {
@@ -22,14 +26,28 @@ class Dashboard extends React.Component {
     });
   }
 
+  toggleCall() {
+    console.log('toggling:  ', this.state.inCall);
+    this.setState({
+      inCall: !this.state.inCall
+    })
+  }
+
   render() {
     return (
       <div className='dashboard'>
         <div className='top'>
           <div className='video-box'>
-            <div className='video-wrapper'>
-              <video id="theirVideo" muted="true" autoPlay="true"></video>
-            </div>
+            <button onClick={this.toggleCall.bind(this)}> Toggle call </button>
+            {this.state.inCall && 
+              <div className='video-wrapper'>
+                <video id="theirVideo" muted="true" autoPlay="true"></video>
+                <h1> Im in a call right now! </h1>
+              </div>
+            }
+            {!this.state.inCall &&
+              <h1> Call is le over </h1>
+            }
           </div>
           <div className='profile'>
             <div className='sign-out'>
