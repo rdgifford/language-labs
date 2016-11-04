b23c9345323f21d245071009341b496c1dac0566
Cheney Mao <cheneymao94@gmail.com>
2016-11-04 15:35:08 -0700

    (feat) Add text submission bar and begin work on message

---
+++ imports/components/ChatMessage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ChatMessage = () => (
+  <div className="chatmessage-container">
+    <div className="chatuser"> aaa </div>
+    <div className="chatMessage"></div>
+  </div>
+);
+
+export default ChatMessage;
--- imports/components/ChatTab.js
+++ imports/components/ChatTab.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import ChatMessage from './ChatMessage';
 
-const ChatTab = () => (
-  <div id="Chat" className="chatbox-container tabcontent">
-    <div className="chatbox-content-container">
 
-    </div>
-    <h1>2</h1>
-  </div>
-);
+class ChatTab extends React.Component {
+  submitHandler(e) {
+    e.preventDefault();
+    // const text = e.currentTarget.children[0].textContent;
+    e.currentTarget.children[0].textContent = '';
+    // if (text !== '') {
+    // }
+  }
+
+  render() {
+    return (
+      <div id="Chat" className="chatbox-container tabcontent">
+        <div className="chatbox-content-container">
+          <div className="chatbox-messages">
+            <ChatMessage />
+          </div>
+          <div className="chatbox-form">
+            <form action="" onSubmit={this.submitHandler}>
+              <input type="text" />
+              <input type="submit" />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ChatTab;
