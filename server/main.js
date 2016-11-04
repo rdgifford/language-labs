f29464972b770ac646972e783dd1438b10eb1e3b
rdgifford <robbie.gifford@gmail.com>
2016-11-03 18:46:03 -0700

    (feat) Add basic recording stream to S3, still need to add streaming back to client

--- server/main.js
+++ server/main.js
@@ -1,13 +1,35 @@
 import { Meteor } from 'meteor/meteor';
 
+let counter = 0;
+
 Meteor.startup(function () {
+  Slingshot.fileRestrictions('uploadToAmazonS3', {
+    allowedFileTypes: null,
+    maxSize: 10 * 1024 * 1024,
+  });
+
+  Slingshot.createDirective('uploadToAmazonS3', Slingshot.S3Storage, {
+    region: 'us-west-1',
+    authorize: () => {
+      return true;
+    },
+    bucket: 'languagedotnext',
+    acl: 'public-read',
+    key: () => {
+      return String(++counter);
+    },
+  });
 
   Meteor.publish('presences', function() {
     return Presences.find({}, { userId: true });
   });
 
-  Meteor.publish('users', function () {
+  Meteor.publish('users', function() {
     return Meteor.users.find({});
+  });
+
+  Meteor.publish('videos', function() {
+    return Meteor.videos.find({});
   });
 
   Meteor.methods({
